9f9fbc7bd474a97fa24a56ec8dab6f480864df76
marwen.saidi <marwen.saidi@orange.com>
2021-05-06 19:23:37 +0200

    test with mockito

--- src/test/java/com/openclassrooms/testing/calcul/service/CalculatorServiceTest.java
+++ src/test/java/com/openclassrooms/testing/calcul/service/CalculatorServiceTest.java
@@ -1,18 +1,34 @@
 package com.openclassrooms.testing.calcul.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.openclassrooms.testing.calcul.domain.Calculator;
 import com.openclassrooms.testing.calcul.domain.model.CalculationModel;
 import com.openclassrooms.testing.calcul.domain.model.CalculationType;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Random;
+
+@ExtendWith(MockitoExtension.class)
 public class CalculatorServiceTest {
 
-	Calculator calculator = new Calculator();
+	@Mock
+	Calculator calculator;
 	// Calculator IS CALLED BY CalculatorService
-	CalculatorService classUnderTest = new CalculatorServiceImpl(calculator);
+	CalculatorService classUnderTest;
+
+	@BeforeEach
+	public void init(){
+		classUnderTest = new CalculatorServiceImpl(calculator);
+	}
 
 	@Test
 	public void add_returnsTheSum_ofTwoPositiveNumbers() {
@@ -36,4 +52,33 @@ public class CalculatorServiceTest {
 				new CalculationModel(CalculationType.ADDITION, 0, 0)).getSolution();
 		assertThat(result).isEqualTo(0);
 	}
+
+	@Test
+	public void calculateThisAddition(){
+		// GIVEN
+		when(calculator.add(1,2)).thenReturn(3);
+
+		// WHEN
+		Integer solution = classUnderTest.calculate(new CalculationModel(CalculationType.ADDITION, 1, 2)).getSolution();
+
+		// THEN
+		verify(calculator).add(1, 2);
+		assertThat(solution).isEqualTo(3);
+	}
+
+	@Test
+	public void calculateAnyAddition(){
+		// GIVEN
+		Random random = new Random();
+		when(calculator.add(any(Integer.class),any(Integer.class))).thenReturn(3);
+
+		// WHEN
+		Integer solution = classUnderTest.calculate(new CalculationModel(CalculationType.ADDITION, random.nextInt(), random.nextInt())).getSolution();
+
+		// THEN
+		verify(calculator).add(any(Integer.class), any(Integer.class));
+		assertThat(solution).isEqualTo(3);
+	}
+
+
 }
